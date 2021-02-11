52a73ebbf018958660e905f3649d7d954e7b7f98
Hayao-H <55943986+Hayao-H@users.noreply.github.com>
2021-02-11 22:51:26 +0900

    [修正]暗号化動画の判定を修正 (#21)

--- Niconicome/Models/Domain/Niconico/Download/Video/VideoDownloader.cs
+++ Niconicome/Models/Domain/Niconico/Download/Video/VideoDownloader.cs
@@ -101,7 +101,7 @@ namespace Niconicome.Models.Domain.Niconico.Download.Video
                     this.messenger.RemoveHandler(onMessage);
                     return this.GetCancelledResult();
                 }
-                await session.EnsureSessionAsync(settings.NiconicoId);
+                await session.EnsureSessionAsync(settings.NiconicoId,true);
 
                 if (!session.IsSessionEnsured)
                 {
