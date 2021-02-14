9d58231cd893a5ccaeb7a16f914e20de25ffc9fd
ShunsukeHKD <shun.hkd@gmail.com>
2021-02-14 19:05:14 +0900

    [追加]Xenoが生成したファイルを解析する機能群を追加

---
+++ Niconicome/Models/Domain/Local/External/Inport/Xeno/XenoPlaylist.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Niconicome.Models.Domain.Local.External.Inport.Xeno
+{
+    public interface IXenoPlaylist
+    {
+        string Name { get; }
+        bool IsRootPlaylist { get; }
+        bool IsChannel { get; }
+        string? ChannelId { get; }
+        int Layer { get; }
+        IXenoPlaylist? ParentPlaylist { get; }
+        List<IXenoPlaylist> ChildPlaylists { get; }
+        List<string> Videos { get; }
+    }
+
+    public interface IXenoVideo
+    {
+        string NiconicoId { get; }
+        string Title { get; }
+        string RelativeFolderPath { get; }
+        int WatchCount { get; }
+        DateTime PostDateTime { get; }
+        string GetAbsoluteFolderPath(string basefolder);
+    }
+
+    /// <summary>
+    /// プレイリスト情報
+    /// </summary>
+    public class XenoPlaylist : IXenoPlaylist
+    {
+        public XenoPlaylist(string name,string channelId,IXenoPlaylist parent)
+        {
+            this.Name = name;
+            this.ChannelId = channelId;
+            this.ParentPlaylist = parent;
+            parent.ChildPlaylists.Add(this);
+        }
+
+        public XenoPlaylist(string name,string channelId)
+        {
+            this.Name = name;
+            this.ChannelId = channelId;
+        }
+
+        public XenoPlaylist(string name,IXenoPlaylist parent)
+        {
+            this.Name = name;
+            this.ParentPlaylist = parent;
+            parent.ChildPlaylists.Add(this);
+        }
+
+        public XenoPlaylist(string name)
+        {
+            this.Name = name;
+        }
+
+        public string Name { get; init; }
+
+        public bool IsRootPlaylist { get => this.ParentPlaylist is null; }
+
+        public bool IsChannel { get => this.ChannelId is not null; }
+
+        public string? ChannelId { get; init; }
+
+        public int Layer
+        {
+            get
+            {
+                var l = 1;
+                IXenoPlaylist playlist = this;
+                while (playlist.ParentPlaylist is not null)
+                {
+                    ++l;
+                    playlist = playlist.ParentPlaylist;
+                }
+                return l;
+            }
+        }
+
+        public IXenoPlaylist? ParentPlaylist { get; init; }
+
+        public List<IXenoPlaylist> ChildPlaylists { get; init; } = new();
+
+        public List<string> Videos { get; init; } = new();
+
+    }
+
+    /// <summary>
+    /// 動画情報
+    /// </summary>
+    public class XenoVideo : IXenoVideo
+    {
+        public XenoVideo(string niconicoId, string title, int watchCount, string relativeFolderPath, DateTime postDatetime)
+        {
+            this.NiconicoId = niconicoId;
+            this.Title = title;
+            this.WatchCount = watchCount;
+            this.RelativeFolderPath = relativeFolderPath;
+            this.PostDateTime = postDatetime;
+
+        }
+
+        public string NiconicoId { get; init; }
+
+        public string Title { get; init; }
+
+        public int WatchCount { get; init; }
+        public string RelativeFolderPath { get; init; }
+
+        public DateTime PostDateTime { get; init; }
+
+
+
+        public string GetAbsoluteFolderPath(string basefolder)
+        {
+            return Path.Combine(basefolder, this.RelativeFolderPath);
+        }
+    }
+}
---
+++ Niconicome/Models/Domain/Local/External/Inport/Xeno/XenoRootParser.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Niconicome.Models.Domain.Utils;
+
+namespace Niconicome.Models.Domain.Local.External.Inport.Xeno
+{
+    public interface IXenoParseResult
+    {
+        int SkippedCount { get; }
+        int SucceededCount { get; }
+        int FailedCount { get; }
+        List<IXenoPlaylist> Playlists { get; }
+    }
+    public interface IXenoRootParser
+    {
+        IXenoParseResult ParseText(string text);
+    }
+
+    public class XenoParseResult : IXenoParseResult
+    {
+        public int SucceededCount { get; set; }
+
+        public int SkippedCount { get; set; }
+
+        public int FailedCount { get; set; }
+
+        public List<IXenoPlaylist> Playlists { get; init; } = new();
+
+    }
+
+    public class XenoRootParser : IXenoRootParser
+    {
+        public XenoRootParser(ILogger logger, IXenoVideoNodeParser videoNodeParser)
+        {
+            this.logger = logger;
+            this.videoNodeParser = videoNodeParser;
+        }
+
+        private readonly ILogger logger;
+
+        private readonly IXenoVideoNodeParser videoNodeParser;
+
+
+        /// <summary>
+        /// 一覧を解析する
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns></returns>
+        public IXenoParseResult ParseText(string text)
+        {
+            var result = new XenoParseResult();
+            var currentParent = new XenoPlaylist("Root");
+
+            foreach (var line in text.Split(Environment.NewLine).Select((content, index) => new { content, index }))
+            {
+                IXenoRootNode node;
+                try
+                {
+                    node = new XenoRootNode(line.content);
+                }
+                catch (Exception e)
+                {
+                    result.FailedCount++;
+                    this.logger.Error($"行の解析に失敗しました。(行:{line.index + 1}, content: {line.content})", e);
+                    continue;
+                }
+
+                var playlist = this.ConvertToXenoPlaylist(node, currentParent, out bool cResult);
+                if (cResult)
+                {
+                    result.Playlists.Add(playlist);
+                    result.SucceededCount++;
+                }
+                else
+                {
+                    result.FailedCount++;
+                }
+
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// ノード情報をプレイリスト情報に変換する
+        /// </summary>
+        /// <param name="node"></param>
+        /// <param name="parent"></param>
+        /// <param name="result"></param>
+        /// <returns></returns>
+        private IXenoPlaylist ConvertToXenoPlaylist(IXenoRootNode node, IXenoPlaylist parent, out bool result)
+        {
+            if (node.Title is null)
+            {
+                result = false;
+                return new XenoPlaylist("Failed");
+            }
+
+            if (node.ListPath is not null)
+            {
+                var listPath = node.ListPath;
+                if (Uri.IsWellFormedUriString(listPath, UriKind.Absolute))
+                {
+                    if (node.ListPath.Contains("ch.nicovideo.jp"))
+                    {
+                        result = true;
+                        var playlist = new XenoPlaylist(node.Title, listPath[(listPath.LastIndexOf("/") + 1)..], parent);
+                        return playlist;
+                    }
+                    else if (File.Exists(node.ListPath))
+                    {
+                        var vResult = this.videoNodeParser.ParseFromFile(node.ListPath);
+                        var playlist = new XenoPlaylist(node.Title, parent);
+                        result = true;
+                        playlist.Videos.AddRange(vResult.Videos.Select(v => v.NiconicoId!));
+                        return playlist;
+                    }
+                }
+            }
+
+            result = true;
+            return new XenoPlaylist(node.Title,parent);
+        }
+    }
+}
