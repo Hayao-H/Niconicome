f1fc49ad1a58e31403ff6337343899035bf385a4
Hayao-H <55943986+Hayao-H@users.noreply.github.com>
2021-03-07 15:30:36 +0900

    [追加]ダウンロード設定ページを追加

---
+++ Niconicome/ViewModels/Setting/Pages/DownloadSettingPageViewModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WS = Niconicome.Workspaces;
+using Local = Niconicome.Models.Local;
+using System.ComponentModel;
+
+namespace Niconicome.ViewModels.Setting.Pages
+{
+    class DownloadSettingPageViewModel : SettingaBase
+    {
+        public DownloadSettingPageViewModel()
+        {
+            var cOffset = WS::SettingPage.SettingHandler.GetIntSetting(Local::Settings.CommentOffset);
+            if (cOffset == -1)
+            {
+                this.commentOffsetField = 40;
+            } else
+            {
+                this.commentOffsetField = cOffset;
+            }
+        }
+
+        private int commentOffsetField;
+
+        /// <summary>
+        /// コメントのオフセット
+        /// </summary>
+        public int CommentOffsetFIeld
+        {
+            get => this.commentOffsetField;
+            set
+            {
+                if (!int.TryParse(value.ToString(), out int _)) return;
+                this.Savesetting(ref this.commentOffsetField, value, Local::Settings.CommentOffset);
+            }
+        }
+
+    }
+
+    [Obsolete("Desinger", true)]
+    class DownloadSettingPageViewModelD
+    {
+        public int CommentOffsetFIeld { get; set; } = 40;
+    }
+}
