9d194785baab8aec7ede9934bc0b729ee758a8e8
Hayao-H <55943986+Hayao-H@users.noreply.github.com>
2021-03-26 22:08:12 +0900

    [追加]検索結果登録機能を追加  (#132)
    * [追加]検索結果登録機能を追加
    
    * [変更]メッセージング処理を追加
    
    * [追加]同時実行を制限

--- Niconicome/Models/Network/VideoIDHandler.cs
+++ Niconicome/Models/Network/VideoIDHandler.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 using Niconicome.Models.Domain.Local.LocalFile;
+using Niconicome.Models.Domain.Niconico.Search;
 using Niconicome.Models.Domain.Utils;
 using Niconicome.Models.Playlist;
 
@@ -12,6 +14,8 @@ namespace Niconicome.Models.Network
     {
         Task<IEnumerable<IVideoListInfo>> GetVideoListInfosAsync(string inputText, IEnumerable<string> registeredVideos, Action<string> onMessage, Action<string> onMessageVerbose);
         Task<INetworkResult> TryGetVideoListInfosAsync(List<IVideoListInfo> videos, string inputText, IEnumerable<string> registeredVideos, Action<string> onMessage, Action<string> onMessageVerbose);
+        bool IsProcessing { get; }
+        event EventHandler? StateChange;
     }
 
     class VideoIDHandler : IVideoIDHandler
@@ -32,6 +36,26 @@ namespace Niconicome.Models.Network
 
         private readonly IRemotePlaylistHandler remotePlaylistHandler;
 
+        private bool isProcessingField;
+
+        /// <summary>
+        /// 処理中フラグ
+        /// </summary>
+        public bool IsProcessing
+        {
+            get => this.isProcessingField;
+            private set
+            {
+                this.isProcessingField = value;
+                this.RaiseStatechange();
+            }
+        }
+
+        /// <summary>
+        /// 状態変更イベント
+        /// </summary>
+        public event EventHandler? StateChange;
+
         /// <summary>
         /// 動画情報を取得する
         /// </summary>
@@ -41,14 +65,17 @@ namespace Niconicome.Models.Network
         /// <returns></returns>
         public async Task<IEnumerable<IVideoListInfo>> GetVideoListInfosAsync(string inputText, IEnumerable<string> registeredVideos, Action<string> onMessage, Action<string> onMessageVerbose)
         {
+            this.IsProcessing = true;
+
             inputText = inputText.Trim();
             var videos = new List<IVideoListInfo>();
 
             if (Path.IsPathRooted(inputText))
             {
                 onMessage("ローカルディレクトリーから動画を取得します");
-                var retlieved = await this.GetVideoListInfosFromLocalPath(inputText, (_, _, _) => { }, (_, _) => { }, (_, _) => { }, (_) => { });
+                var retlieved = await this.GetVideoListInfosFromLocalPath(inputText, onMessageVerbose);
                 videos.AddRange(retlieved);
+                this.IsProcessing = false;
                 return videos;
             }
             else if (inputText.StartsWith("http"))
@@ -65,14 +92,28 @@ namespace Niconicome.Models.Network
                     onMessage("ネットワークから動画を取得します");
                     var retlieved = await this.GetVideoListInfosFromRemote(registeredVideos, type, id, (m) => onMessageVerbose(m));
                     videos.AddRange(retlieved);
+                    this.IsProcessing = false;
                     return videos;
                 }
             }
 
-            onMessage("IDを登録します");
-            var retlievedId = await this.GetVideoListInfosFromID(inputText, (_, _, _) => { }, (_, _) => { }, (_, _) => { }, (_) => { });
-            videos.AddRange(retlievedId);
-            return videos;
+            if (this.niconicoUtils.IsNiconicoID(inputText))
+            {
+                onMessage("IDを登録します");
+                var retlievedId = await this.GetVideoListInfosFromID(inputText, onMessageVerbose);
+                videos.AddRange(retlievedId);
+                this.IsProcessing = false;
+                return videos;
+            }
+            else
+            {
+                onMessage("動画を検索します");
+                var retlievedId = await this.GetVideoListInfosFromSearchResult(registeredVideos, inputText, onMessageVerbose);
+                videos.AddRange(retlievedId);
+                this.IsProcessing = false;
+                return videos;
+            }
+
         }
 
         /// <summary>
@@ -90,7 +131,8 @@ namespace Niconicome.Models.Network
             {
                 var retlieved = await this.GetVideoListInfosAsync(inputText, registeredVideos, onMessage, onMessageVerbose);
                 videos.AddRange(retlieved);
-            } catch
+            }
+            catch
             {
                 var result = new NetworkResult()
                 {
@@ -100,11 +142,20 @@ namespace Niconicome.Models.Network
                 return result;
             }
 
-             return new NetworkResult();
+            return new NetworkResult();
 
 
         }
 
+        /// <summary>
+        /// 状態変更イベントを発火させる
+        /// </summary>
+        private void RaiseStatechange()
+        {
+            this.StateChange?.Invoke(this, EventArgs.Empty);
+        }
+
+        #region 内部処理
         /// <summary>
         /// ローカルフォルダーから動画情報を取得する
         /// </summary>
@@ -114,11 +165,26 @@ namespace Niconicome.Models.Network
         /// <param name="onStarted"></param>
         /// <param name="onWaiting"></param>
         /// <returns></returns>
-        private async Task<IEnumerable<IVideoListInfo>> GetVideoListInfosFromLocalPath(string localPath, Action<IVideoListInfo, IVideoListInfo, int> onSucceeded, Action<IResult, IVideoListInfo> onFailed, Action<IVideoListInfo, int> onStarted, Action<IVideoListInfo> onWaiting)
+        private async Task<IEnumerable<IVideoListInfo>> GetVideoListInfosFromLocalPath(string localPath, Action<string> onMessage)
         {
             var ids = this.localDirectoryHandler.GetVideoIdsFromDirectory(localPath);
+            var videoCount = ids.Count();
 
-            var videos = await this.networkVideoHandler.GetVideoListInfosAsync(ids, onSucceeded, onFailed, onStarted, onWaiting);
+            var videos = await this.networkVideoHandler.GetVideoListInfosAsync(ids, (newVideo, video, i) =>
+            {
+                onMessage($"{video.NiconicoId}の情報を取得しました。({i + 1}/{videoCount})");
+
+            }, (result, video) =>
+            {
+                onMessage($"{video.NiconicoId}の情報を更新に失敗しました。(詳細: {result.Message ?? "None"})");
+            }, (video, i) =>
+            {
+                onMessage($"情報を取得中...({i + 1}/{videoCount})");
+            },
+            video =>
+            {
+                onMessage("待機中...(15s)");
+            });
 
             return videos;
         }
@@ -132,9 +198,23 @@ namespace Niconicome.Models.Network
         /// <param name="onStarted"></param>
         /// <param name="onWaiting"></param>
         /// <returns></returns>
-        private async Task<IEnumerable<IVideoListInfo>> GetVideoListInfosFromID(string id, Action<IVideoListInfo, IVideoListInfo, int> onSucceeded, Action<IResult, IVideoListInfo> onFailed, Action<IVideoListInfo, int> onStarted, Action<IVideoListInfo> onWaiting)
+        private async Task<IEnumerable<IVideoListInfo>> GetVideoListInfosFromID(string id, Action<string> onMessage)
         {
-            var videos = await this.networkVideoHandler.GetVideoListInfosAsync(new List<string>() { id }, onSucceeded, onFailed, onStarted, onWaiting);
+            var videos = await this.networkVideoHandler.GetVideoListInfosAsync(new List<string>() { id }, (newVideo, video, i) =>
+            {
+                onMessage($"{video.NiconicoId}の情報を取得しました。");
+
+            }, (result, video) =>
+            {
+                onMessage($"{video.NiconicoId}の情報を更新に失敗しました。(詳細: {result.Message ?? "None"})");
+            }, (video, i) =>
+            {
+                onMessage($"情報を取得中...");
+            },
+            video =>
+            {
+                onMessage("待機中...(15s)");
+            });
 
             return videos;
         }
@@ -155,5 +235,50 @@ namespace Niconicome.Models.Network
 
             return videos;
         }
+
+        /// <summary>
+        /// 動画を検索する
+        /// </summary>
+        /// <param name="registeredVideos"></param>
+        /// <param name="keyword"></param>
+        /// <param name="onSucceeded"></param>
+        /// <param name="onFailed"></param>
+        /// <param name="onStarted"></param>
+        /// <param name="onWaiting"></param>
+        /// <returns></returns>
+        private async Task<IEnumerable<IVideoListInfo>> GetVideoListInfosFromSearchResult(IEnumerable<string> registeredVideos, string keyword, Action<string> onMessage)
+        {
+            IEnumerable<IVideoListInfo> videos = new List<IVideoListInfo>();
+
+            var searchResult = await this.remotePlaylistHandler.TrySearchVideosAsync(keyword, SearchType.Keyword, 1);
+
+            if (!searchResult.IsSucceeded || searchResult.Videos is null)
+            {
+                onMessage($"検索に失敗しました(詳細: {searchResult.Message ?? "none"})");
+                return videos;
+            }
+
+            int videoCount = searchResult.Videos.Count();
+
+            videos = await this.networkVideoHandler.GetVideoListInfosAsync(searchResult.Videos.Select(v => v.Id).Where(v => !registeredVideos.Contains(v)), (newVideo, video, i) =>
+            {
+                onMessage($"{video.NiconicoId}の情報を取得しました。({i + 1}/{videoCount})");
+
+            }, (result, video) =>
+            {
+                onMessage($"{video.NiconicoId}の情報を更新に失敗しました。(詳細: {result.Message ?? "None"})");
+            }, (video, i) =>
+            {
+                onMessage($"情報を取得中...({i + 1}/{videoCount})");
+            },
+            video =>
+            {
+                onMessage("待機中...(15s)");
+            });
+
+            return videos;
+
+        }
+        #endregion
     }
 }
