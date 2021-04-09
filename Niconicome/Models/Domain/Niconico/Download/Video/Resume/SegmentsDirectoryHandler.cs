0a7276e225b6780fc3a4925bff173fb6a05add7d
Hayao-H <55943986+Hayao-H@users.noreply.github.com>
2021-04-09 22:16:33 +0900

    [追加]セグメントファイルの情報を取得するクラスを追加

---
+++ Niconicome/Models/Domain/Niconico/Download/Video/Resume/SegmentsDirectoryHandler.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Niconicome.Models.Domain.Local.IO;
+
+namespace Niconicome.Models.Domain.Niconico.Download.Video.Resume
+{
+
+    public interface ISegmentsDirectoryHandler
+    {
+        ISegmentsDirectoryInfo GetSegmentsDirectoryInfo(string niconicoID);
+    }
+
+    public class SegmentsDirectoryHandler : ISegmentsDirectoryHandler
+    {
+        public SegmentsDirectoryHandler(INicoDirectoryIO directoryIO)
+        {
+            this.directoryIO = directoryIO;
+        }
+
+        /// <summary>
+        /// ディレクトリを操作する
+        /// </summary>
+        private readonly INicoDirectoryIO directoryIO;
+
+        /// <summary>
+        /// セグメントファイルの情報を取得する
+        /// </summary>
+        /// <param name="niconicoID"></param>
+        /// <returns></returns>
+        public ISegmentsDirectoryInfo GetSegmentsDirectoryInfo(string niconicoID)
+        {
+            var dirs = this.directoryIO.GetDirectorys(@"\tmp\", $"{niconicoID}-*");
+
+            if (dirs.Count == 0) throw new IOException($"{niconicoID}のセグメントファイルは保存されていません。");
+
+            var dir = dirs.First();
+            var splitedDirName = dir.Split("-");
+
+            if (splitedDirName.Length < 3) throw new InvalidOperationException($"セグメントファイルディレクトリ名が不正です({dir})");
+
+            var parseResult = uint.TryParse(splitedDirName[1], out uint resolution);
+            if (!parseResult) throw new InvalidOperationException($"セグメントファイルディレクトリ名が不正です({dir})");
+
+
+            var files = this.directoryIO.GetFiles(dir, "*.ts");
+            files.Remove("combined.ts");
+
+            var info = new SegmntsDirectoryInfo(niconicoID, resolution, dir);
+            info.ExistsFileNames.AddRange(files);
+
+            return info;
+        }
+
+    }
+}
---
+++ NiconicomeTest/NetWork/Download/Video/Resume/SegmentDirectoryHandlerUnitTest.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Niconicome.Models.Domain.Local.IO;
+using Niconicome.Models.Domain.Niconico.Download.Video.Resume;
+using NiconicomeTest.Stabs.Models.Domain.Local.IO;
+using NUnit.Framework;
+
+namespace NiconicomeTest.NetWork.Download.Video.Resume
+{
+
+    [TestFixture]
+    class SegmentDirectoryHandlerUnitTest
+    {
+        private INicoDirectoryIO? nicoDirectoryIO;
+
+        private ISegmentsDirectoryHandler? segmentsDirectoryHandler;
+
+        [Test]
+        public void 存在しないディレクトリを渡す()
+        {
+            this.nicoDirectoryIO = new NicoDirectoryIOMock(_ => false, (_, _, _) => new List<string>(), (_, _, _) => new List<string>());
+            this.segmentsDirectoryHandler = new SegmentsDirectoryHandler(this.nicoDirectoryIO);
+
+            Assert.That(() => this.segmentsDirectoryHandler.GetSegmentsDirectoryInfo("sm9"), Throws.InstanceOf<IOException>());
+        }
+
+        [Test]
+        public void 不正なパスを渡す()
+        {
+            this.nicoDirectoryIO = new NicoDirectoryIOMock(_ => false, (_, _, _) => new List<string>(), (_, _, _) => new List<string>() { "sm9-hoge-2021-04-09" });
+            this.segmentsDirectoryHandler = new SegmentsDirectoryHandler(this.nicoDirectoryIO);
+
+            Assert.That(() => this.segmentsDirectoryHandler.GetSegmentsDirectoryInfo("sm9"), Throws.InstanceOf<InvalidOperationException>());
+        }
+
+        [Test]
+        public void セグメント0個()
+        {
+            this.nicoDirectoryIO = new NicoDirectoryIOMock(_ => false, (_, _, _) => new List<string>(), (_, _, _) => new List<string>() { "sm9-1080-2021-04-09" });
+            this.segmentsDirectoryHandler = new SegmentsDirectoryHandler(this.nicoDirectoryIO);
+            var result = this.segmentsDirectoryHandler.GetSegmentsDirectoryInfo("sm9");
+
+            Assert.That(result.DirectoryName, Is.EqualTo("sm9-1080-2021-04-09"));
+            Assert.That(result.ExistsFileNames.Count, Is.EqualTo(0));
+            Assert.That(result.Resolution, Is.EqualTo((uint)1080));
+            Assert.That(result.NiconicoID, Is.EqualTo("sm9"));
+
+        }
+
+        [Test]
+        public void セグメント3個()
+        {
+            this.nicoDirectoryIO = new NicoDirectoryIOMock(_ => false, (_, _, _) => new List<string>() { "1.ts", "2.ts", "3.ts", "combined.ts" }, (_, _, _) => new List<string>() { "sm9-1080-2021-04-09" });
+            this.segmentsDirectoryHandler = new SegmentsDirectoryHandler(this.nicoDirectoryIO);
+            var result = this.segmentsDirectoryHandler.GetSegmentsDirectoryInfo("sm9");
+
+            Assert.That(result.DirectoryName, Is.EqualTo("sm9-1080-2021-04-09"));
+            Assert.That(result.ExistsFileNames.Count, Is.EqualTo(3));
+            Assert.That(result.Resolution, Is.EqualTo((uint)1080));
+            Assert.That(result.NiconicoID, Is.EqualTo("sm9"));
+
+        }
+    }
+}
