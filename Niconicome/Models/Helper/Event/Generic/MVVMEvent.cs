08ba3059c937c34ecf3ea5c44425964d54e8e8f5
Hayao-H <55943986+Hayao-H@users.noreply.github.com>
2021-05-02 14:02:22 +0900

    [追加]イベント送受信のための共通クラスを定義

---
+++ Niconicome/Models/Helper/Event/Generic/MVVMEvent.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Niconicome.Models.Helper.Event.Generic
+{
+    public interface IViewModelEvent<T>
+    {
+        T Data { get; }
+        Type TargetVMType { get; }
+        bool CheckTarget(Type receiversType, object? sender = null);
+    }
+
+    class MVVMEvent<T> : IViewModelEvent<T>
+    {
+        public MVVMEvent(T data, Type targetType, Func<object, bool>? additionalChecker = null)
+        {
+            this.Data = data;
+            this.TargetVMType = targetType;
+            this.additionalChecker = additionalChecker;
+        }
+
+        public T Data { get; init; }
+
+        public Type TargetVMType { get; init; }
+
+        private readonly Func<object, bool>? additionalChecker;
+
+        /// <summary>
+        /// 自分が正しい受け取りてであるかどうかをチェックする
+        /// </summary>
+        /// <param name="receiversType"></param>
+        /// <returns></returns>
+        public bool CheckTarget(Type receiversType, object? sender = null)
+        {
+            if (receiversType != this.TargetVMType)
+            {
+                return false;
+            }
+            else if (this.additionalChecker is not null && sender is not null)
+            {
+                return this.additionalChecker(sender);
+            }
+
+            return true;
+        }
+    }
+}
