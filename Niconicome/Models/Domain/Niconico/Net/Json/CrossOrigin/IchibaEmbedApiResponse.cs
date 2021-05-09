f6913c253cc88b118839787bfbd98c5b1cf574d1
Hayao-H <55943986+Hayao-H@users.noreply.github.com>
2021-05-09 21:19:19 +0900

    [追加]市場APIからのレスポンス型情報を追加

---
+++ Niconicome/Models/Domain/Niconico/Net/Json/CrossOrigin/IchibaEmbedApiResponse.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Niconicome.Models.Domain.Niconico.Net.Json.CrossOrigin
+{
+    namespace V3
+    {
+        public class IchibaEmbedApiResponse
+        {
+            [JsonPropertyName("main")]
+            public string Main { get; set; } = string.Empty;
+        }
+    }
+}
