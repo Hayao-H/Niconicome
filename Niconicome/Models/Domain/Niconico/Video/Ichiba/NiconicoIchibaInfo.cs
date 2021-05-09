63217cd721a95c5de5bcc584cb5114b384ad65a6
Hayao-H <55943986+Hayao-H@users.noreply.github.com>
2021-05-09 21:25:26 +0900

    [追加]Model用の商品型を追加

---
+++ Niconicome/Models/Domain/Niconico/Video/Ichiba/NiconicoIchibaInfo.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Niconicome.Models.Domain.Niconico.Video.Ichiba
+{
+
+    public interface IIchibaItem
+    {
+        string Name { get; }
+        string Category { get; }
+        string Price { get; }
+        string LinkUrl { get; }
+    }
+
+    public interface INiconicoIchibaInfo
+    {
+        List<IIchibaItem> IchibaItems { get; }
+    }
+
+    /// <summary>
+    /// 商品情報のラッパー
+    /// </summary>
+    class NiconicoIchibaInfo: INiconicoIchibaInfo
+    {
+        public List<IIchibaItem> IchibaItems { get; init; } = new();
+    }
+
+    /// <summary>
+    /// 商品情報
+    /// </summary>
+    class IchibaItem : IIchibaItem
+    {
+        public string Name { get; set; } = string.Empty;
+
+       public string Category { get; set; } = string.Empty;
+
+        public string Price { get; set; } = string.Empty;
+
+        public string LinkUrl { get; set; } = string.Empty;
+    }
+}
