7a0d0ef0f3eebd61d4c981dfbef7457ebf5d73bf
Hayao-H <55943986+Hayao-H@users.noreply.github.com>
2021-06-03 23:03:46 +0900

    [追加]互換性のチェック機能を追加

---
+++ Niconicome/Models/Utils/Compatibility.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Niconicome.Models.Utils
+{
+    public static class Compatibility
+    {
+        private static Version? osVersion;
+
+        /// <summary>
+        /// OSのバージョンを返す
+        /// </summary>
+        public static Version OsVersion => Compatibility.osVersion ?? (Compatibility.osVersion = Environment.OSVersion.Version);
+
+        /// <summary>
+        /// OSのバージョンを比較する
+        /// </summary>
+        /// <param name="major"></param>
+        /// <param name="minor"></param>
+        /// <param name="build"></param>
+        /// <returns></returns>
+        public static bool IsOsVersionLargerThan(int major, int minor = 0, int build = 0)
+        {
+            var version = new Version(major, minor, build);
+            return Compatibility.OsVersion.CompareTo(version) >= 0;
+        }
+    }
+}
