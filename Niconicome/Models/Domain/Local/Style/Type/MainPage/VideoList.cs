8db2c5b9ed5cfbef247a6c8a54412df796bba8cf
Hayao-H <55943986+Hayao-H@users.noreply.github.com>
2021-06-15 20:47:15 +0900

    [追加]列設定を追加

--- Niconicome/Models/Domain/Local/Style/Type/MainPage/VideoList.cs
+++ Niconicome/Models/Domain/Local/Style/Type/MainPage/VideoList.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.Json.Serialization;
+﻿using System.Reactive.Disposables;
+using System.Text.Json.Serialization;
 
 namespace Niconicome.Models.Domain.Local.Style.Type.MainPage
 {
@@ -16,5 +17,32 @@ namespace Niconicome.Models.Domain.Local.Style.Type.MainPage
 
         [JsonPropertyName("buttonsHeight")]
         public int ButtonsHeight { get; set; } = 50;
+
+        [JsonPropertyName("column")]
+        public Column Column { get; set; } = new();
+    }
+
+    public class Column
+    {
+        [JsonPropertyName("thumbnail")]
+        public bool Thumbnail { get; set; } = true;
+
+        [JsonPropertyName("niconicoID")]
+        public bool NiconicoID { get; set; } = true;
+
+        [JsonPropertyName("title")]
+        public bool Title { get; set; } = true;
+
+        [JsonPropertyName("uploadedDT")]
+        public bool UploadedDT { get; set; } = true;
+
+        [JsonPropertyName("viewCount")]
+        public bool ViewCount { get; set; } = true;
+
+        [JsonPropertyName("dlFlag")]
+        public bool DLFlag { get; set; } = true;
+
+        [JsonPropertyName("bookMark")]
+        public bool BookMark { get; set; } = true;
     }
 }
