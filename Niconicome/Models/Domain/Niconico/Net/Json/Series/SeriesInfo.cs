d4ca716fa0084e24919e06af009bdb039018264b
Hayao-H <55943986+Hayao-H@users.noreply.github.com>
2021-06-21 22:06:02 +0900

    [追加]シリーズの型情報を追加

---
+++ Niconicome/Models/Domain/Niconico/Net/Json/Series/SeriesInfo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace Niconicome.Models.Domain.Niconico.Net.Json.Series
+{
+
+
+    public partial class Temperatures
+    {
+        [JsonPropertyName("itemListElement")]
+        public List<ItemListElement> ItemListElement { get; set; } = new();
+    }
+
+    public partial class ItemListElement
+    {
+
+        [JsonPropertyName("name")]
+        public string Name { get; set; } = string.Empty;
+
+        [JsonPropertyName("url")]
+        public string Url { get; set; } = string.Empty;
+
+        [JsonPropertyName("uploadDate")]
+        public DateTimeOffset UploadDate { get; set; } = DateTime.Now;
+
+        [JsonPropertyName("thumbnailUrl")]
+        public List<Uri> ThumbnailUrl { get; set; } = new();
+    }
+
+}
