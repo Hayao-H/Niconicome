5115d9902e61f9277a76e381982f30bb7592fca9
Hayao-H <55943986+Hayao-H@users.noreply.github.com>
2021-07-04 22:13:43 +0900

    [追加]マニフェストファイルを追加

---
+++ Niconicome/Models/Domain/Local/Addons/Manifest/ManifestBase.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Niconicome.Models.Domain.Local.Addons.Manifest
+{
+    abstract class ManifestBase
+    {
+        public virtual string ManifestVersion { get; set; } = string.Empty;
+    }
+}
---
+++ Niconicome/Models/Domain/Local/Addons/Manifest/V1/Manifest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Niconicome.Models.Domain.Local.Addons.Manifest.V1
+{
+    class Manifest : ManifestBase
+    {
+        public override string ManifestVersion { get; set; } = "1.0";
+
+        public string Name { get; set; } = string.Empty;
+
+        public string Author { get; set; } = string.Empty;
+
+        public string Description { get; set; } = string.Empty;
+
+        public string Version { get; set; } = "1.0.0";
+
+        public AutoUpdatePolicy AutoUpdatePolicy { get; set; } = new();
+
+        public List<string> Permissions { get; set; } = new();
+    }
+
+    class AutoUpdatePolicy
+    {
+        public bool AutoUpdate { get; set; }
+
+        public bool GithubRelease { get; set; }
+
+        public string GithubReleaseFileName { get; set; } = string.Empty;
+
+        public string GithubOwnerName { get; set; } = string.Empty;
+
+        public string GithubRepoName { get; set; } = string.Empty;
+    }
+
+    class Scripts
+    {
+        public string BackgroundScript { get; set; } = string.Empty;
+    }
+}
