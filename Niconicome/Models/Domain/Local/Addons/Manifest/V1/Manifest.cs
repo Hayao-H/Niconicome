98d53f4ae1c5073d62af0fa30347b346587b6961
Hayao-H <55943986+Hayao-H@users.noreply.github.com>
2021-07-05 20:08:55 +0900

    [変更]プロパティー名を変更

--- Niconicome/Models/Domain/Local/Addons/Manifest/V1/Manifest.cs
+++ Niconicome/Models/Domain/Local/Addons/Manifest/V1/Manifest.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace Niconicome.Models.Domain.Local.Addons.Manifest.V1
 {
     class Manifest : ManifestBase
     {
+        [JsonPropertyName("manifest_version")]
         public override string ManifestVersion { get; set; } = "1.0";
 
         public string Name { get; set; } = string.Empty;
@@ -25,19 +27,25 @@ namespace Niconicome.Models.Domain.Local.Addons.Manifest.V1
 
     class AutoUpdatePolicy
     {
+        [JsonPropertyName("auto_update")]
         public bool AutoUpdate { get; set; }
 
+        [JsonPropertyName("github_release")]
         public bool GithubRelease { get; set; }
 
-        public string GithubReleaseFileName { get; set; } = string.Empty;
+        [JsonPropertyName("github_release_filename_pattern")]
+        public string GithubReleaseFileNamePattern { get; set; } = string.Empty;
 
+        [JsonPropertyName("github_owner_name")]
         public string GithubOwnerName { get; set; } = string.Empty;
 
+        [JsonPropertyName("github_repo_name")]
         public string GithubRepoName { get; set; } = string.Empty;
     }
 
     class Scripts
     {
+        [JsonPropertyName("background_script")]
         public string BackgroundScript { get; set; } = string.Empty;
     }
 }
