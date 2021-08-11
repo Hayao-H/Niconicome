ead54ae5b3bdfd174a3c7e6ea451116555bd64ad
Hayao-H <55943986+Hayao-H@users.noreply.github.com>
2021-08-11 21:42:46 +0900

    [追加]タイマーのGUIを追加

---
+++ Niconicome/ViewModels/Mainpage/TimerSettingsViewModel.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Niconicome.Models.Network.Download.Actions;
+using Niconicome.ViewModels.Mainpage.Utils;
+using Reactive.Bindings;
+
+namespace Niconicome.ViewModels.Mainpage
+{
+    class TimerSettingsViewModel : BindableBase
+    {
+        public TimerSettingsViewModel()
+        {
+            #region actions
+
+            var none = new ComboboxItem<PostDownloadActions>(PostDownloadActions.None, "何もしない");
+            var shutdown = new ComboboxItem<PostDownloadActions>(PostDownloadActions.Shutdown, "シャットダウン");
+            var restart = new ComboboxItem<PostDownloadActions>(PostDownloadActions.Restart, "再起動");
+            var logoff = new ComboboxItem<PostDownloadActions>(PostDownloadActions.LogOff, "ログオフ");
+            var sleep = new ComboboxItem<PostDownloadActions>(PostDownloadActions.Sleep, "休止状態");
+
+            this.Action = new ReactiveProperty<ComboboxItem<PostDownloadActions>>(none);
+            this.SelectableActions = new List<ComboboxItem<PostDownloadActions>>() { none, shutdown, restart, logoff, sleep };
+
+            #endregion
+        }
+
+        #region Props
+
+        public ReactiveProperty<ComboboxItem<PostDownloadActions>> Action { get; init; }
+
+        public List<ComboboxItem<PostDownloadActions>> SelectableActions { get; init; }
+
+        #endregion
+    }
+
+    class TimerSettingsViewModelD
+    {
+
+        public TimerSettingsViewModelD()
+        {
+            #region actions
+
+            var none = new ComboboxItem<PostDownloadActions>(PostDownloadActions.None, "何もしない");
+            var shutdown = new ComboboxItem<PostDownloadActions>(PostDownloadActions.Shutdown, "シャットダウン");
+            var restart = new ComboboxItem<PostDownloadActions>(PostDownloadActions.Restart, "再起動");
+            var logoff = new ComboboxItem<PostDownloadActions>(PostDownloadActions.LogOff, "ログオフ");
+            var sleep = new ComboboxItem<PostDownloadActions>(PostDownloadActions.Sleep, "休止状態");
+
+            this.Action = new ReactiveProperty<ComboboxItem<PostDownloadActions>>(none);
+            this.SelectableActions = new List<ComboboxItem<PostDownloadActions>>() { none, shutdown, restart, logoff, sleep };
+
+            #endregion
+        }
+
+
+        public ReactiveProperty<ComboboxItem<PostDownloadActions>> Action { get; init; }
+
+        public List<ComboboxItem<PostDownloadActions>> SelectableActions { get; init; }
+
+    }
+}
---
+++ Niconicome/Views/Mainpage/Region/TimerSettings.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Niconicome.ViewModels;
+using Niconicome.ViewModels.Mainpage;
+
+namespace Niconicome.Views.Mainpage.Region
+{
+    /// <summary>
+    /// TimerSettings.xaml の相互作用ロジック
+    /// </summary>
+    [ViewModel(typeof(TimerSettingsViewModel))]
+    public partial class TimerSettings : UserControl
+    {
+        public TimerSettings()
+        {
+            InitializeComponent();
+        }
+    }
+}
