412ae0d58646103c743bb3cadfa1b66e4d61f1e1
Hayao-H <55943986+Hayao-H@users.noreply.github.com>
2021-08-24 22:12:20 +0900

    [変更]Session APIに対応

---
+++ Niconicome/Models/Domain/Niconico/Watch/WatchSessionInfo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Niconicome.Models.Domain.Niconico.Watch
+{
+    public interface IWatchSessionInfo
+    {
+        string DmcResponseJsonData { get; }
+        string ContentUrl { get; }
+        string SessionId { get; }
+    }
+
+    /// <summary>
+    /// セッション情報
+    /// </summary>
+    public class WatchSessionInfo : IWatchSessionInfo
+    {
+        public string DmcResponseJsonData { get; set; } = string.Empty;
+        public string ContentUrl { get; set; } = string.Empty;
+        public string SessionId { get; set; } = string.Empty;
+
+    }
+}
