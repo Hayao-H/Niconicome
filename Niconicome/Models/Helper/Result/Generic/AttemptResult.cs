b61bc036f1cf3efbe41458b76370597c9ebdb55e
Hayao-H <55943986+Hayao-H@users.noreply.github.com>
2021-11-23 21:43:29 +0900

    [変更]new系のメソッドを追加

--- Niconicome/Models/Helper/Result/Generic/AttemptResult.cs
+++ Niconicome/Models/Helper/Result/Generic/AttemptResult.cs
@@ -1,15 +1,37 @@
-﻿using Result = Niconicome.Models.Helper.Result;
+﻿using System;
+using Result = Niconicome.Models.Helper.Result;
 
 namespace Niconicome.Models.Helper.Result
 {
 
     public interface IAttemptResult<T> : Result::IAttemptResult
     {
+        /// <summary>
+        /// 情報
+        /// </summary>
         T? Data { get; }
     }
 
     public class AttemptResult<T> : Result::AttemptResult, IAttemptResult<T>
     {
         public T? Data { get; set; }
+
+        /// <summary>
+        /// インスタンスを作成
+        /// </summary>
+        /// <returns></returns>
+        public static IAttemptResult<T> Fail(string? message = null, Exception? ex = null)
+        {
+            return new AttemptResult<T>() { Message = message, Exception = ex};
+        }
+
+        /// <summary>
+        /// インスタンスを作成
+        /// </summary>
+        /// <returns></returns>
+        public static IAttemptResult<T> Succeeded()
+        {
+            return new AttemptResult<T>() { IsSucceeded = true };
+        }
     }
 }
