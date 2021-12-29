2cef737f31c27f6515447cfa10d8ddb2c0840607
Hayao-H <55943986+Hayao-H@users.noreply.github.com>
2021-12-29 21:31:27 +0900

    [追加]コンバーターを追加

---
+++ Niconicome/ViewModels/Converter/KeyDown/KeyDownEventToKeyEventInfoConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Reactive.Bindings.Interactivity;
+using System.Reactive.Linq;
+
+namespace Niconicome.ViewModels.Converter.KeyDown
+{
+    internal class KeyDownEventToKeyEventInfoConverter : ReactiveConverter<KeyEventArgs, KeyEventInfo>
+    {
+        protected override IObservable<KeyEventInfo> OnConvert(IObservable<KeyEventArgs> source)
+        {
+            return source.Select(e => new KeyEventInfo(e.Key));
+        }
+    }
+}
