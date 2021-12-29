2a95d462fda9479eacc5499fb6e5aa1e72882df2
Hayao-H <55943986+Hayao-H@users.noreply.github.com>
2021-12-29 21:33:12 +0900

    Feature/intelli input enter key (#464)
    * [追加]コンバーターを追加
    
    * [変更]エンターキー検知機能を追加

---
+++ Niconicome/ViewModels/Converter/KeyDown/KeyEventInfo.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Niconicome.ViewModels.Converter.KeyDown
+{
+    public record KeyEventInfo(Key Key);
+}
