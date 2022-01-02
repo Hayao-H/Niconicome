505f3d69b1b18362b5a76ce3189236c8773141d2
Hayao-H <55943986+Hayao-H@users.noreply.github.com>
2022-01-02 20:58:37 +0900

    [変更]プロパティーをRPに移行

--- Niconicome/ViewModels/Setting/Pages/GeneralSettingsPageViewModel.cs
+++ Niconicome/ViewModels/Setting/Pages/GeneralSettingsPageViewModel.cs
@@ -20,18 +20,11 @@ namespace Niconicome.ViewModels.Setting.Pages
         public GeneralSettingsPageViewModel()
         {
             #region 自動ログイン
-            this.isAutologinEnableField = WS::SettingPage.SettingHandler.GetBoolSetting(SettingsEnum.AutologinEnable);
+            this.IsAutologinEnable = WS::SettingPage.SettingsContainer.GetReactiveBoolSetting(SettingsEnum.AutologinEnable);
 
             this.SelectableFirefoxProfiles = WS::SettingPage.AutoLogin.GetFirefoxProfiles().Select(p => p.ProfileName).ToList();
             var profile = WS::SettingPage.SettingHandler.GetStringSetting(SettingsEnum.FFProfileName) ?? string.Empty;
-            this.SelectedFirefoxProfileName = new ReactiveProperty<string?>(this.SelectableFirefoxProfiles.FirstOrDefault(p => p == profile));
-            this.SelectedFirefoxProfileName.Subscribe(value =>
-            {
-                if (value is not null)
-                {
-                    this.SaveSetting(value, SettingsEnum.FFProfileName);
-                }
-            });
+            this.SelectedFirefoxProfileName = WS::SettingPage.SettingsContainer.GetReactiveStringSetting(SettingsEnum.FFProfileName, this.SelectableFirefoxProfiles.FirstOrDefault(p => p == profile) ?? "");
 
             var normal = new ComboboxItem<string>(AutoLoginTypeString.Normal, "パスワードログイン");
             var wv2 = new ComboboxItem<string>(AutoLoginTypeString.Webview2, "Webview2とCookieを共有");
@@ -40,18 +33,13 @@ namespace Niconicome.ViewModels.Setting.Pages
             this.SelectableAutoLoginTypes = new List<ComboboxItem<string>>() { normal, wv2, firefox };
 
             //自動ログインのタイプ
-            var type = WS::SettingPage.SettingHandler.GetStringSetting(SettingsEnum.AutologinMode);
-            this.SelectedAutoLoginType = type switch
-            {
-                AutoLoginTypeString.Normal => new ReactiveProperty<ComboboxItem<string>>(normal),
-                AutoLoginTypeString.Webview2 => new ReactiveProperty<ComboboxItem<string>>(wv2),
-                AutoLoginTypeString.Firefox => new ReactiveProperty<ComboboxItem<string>>(firefox),
-                _ => new ReactiveProperty<ComboboxItem<string>>(normal),
-            };
-            this.SelectedAutoLoginType.Subscribe(value =>
-            {
-                this.SaveSetting(value.Value, SettingsEnum.AutologinMode);
-            }).AddTo(this.disposables);
+            this.SelectedAutoLoginType = WS::SettingPage.SettingsContainer.GetReactiveStringSetting(SettingsEnum.AutologinMode).ToReactivePropertyAsSynchronized(x => x.Value, x => x switch
+               {
+                   AutoLoginTypeString.Normal => normal,
+                   AutoLoginTypeString.Webview2 => wv2,
+                   AutoLoginTypeString.Firefox => firefox,
+                   _ => normal,
+               }, x => x.Value);
 
             //Firefoxのプロファイル選択肢を表示するかどうか
             this.DisplayFirefoxPrifile = this.SelectedAutoLoginType
@@ -59,7 +47,6 @@ namespace Niconicome.ViewModels.Setting.Pages
                 .ToReactiveProperty();
             #endregion
 
-            this.isSkippingSSLVerificationEnableField = WS::SettingPage.SettingHandler.GetBoolSetting(SettingsEnum.SkipSSLVerification);
 
             var n1 = new ComboboxItem<int>(1, "1");
             var n2 = new ComboboxItem<int>(2, "2");
@@ -70,20 +57,19 @@ namespace Niconicome.ViewModels.Setting.Pages
             this.SelectablefetchSleepInterval = new List<ComboboxItem<int>> { n1, n2, n3, n4, n5 };
             this.SelectableMaxParallelFetch = new List<ComboboxItem<int>>() { n1, n2, n3, n4 };
 
-
             var maxFetchParallelCount = WS::SettingPage.SettingHandler.GetIntSetting(SettingsEnum.MaxFetchCount);
             var fetchSleepInterval = WS::SettingPage.SettingHandler.GetIntSetting(SettingsEnum.FetchSleepInterval);
 
-            this.maxFetchParallelCountField = maxFetchParallelCount switch
+            this.MaxFetchParallelCount = WS::SettingPage.SettingsContainer.GetReactiveIntSetting(SettingsEnum.MaxFetchCount).ToReactivePropertyAsSynchronized(x => x.Value, x => x switch
             {
                 1 => n1,
                 2 => n2,
                 3 => n3,
                 4 => n4,
                 _ => n4,
-            };
+            }, x => x.Value);
 
-            this.fetchSleepIntervalFIeld = fetchSleepInterval switch
+            this.FetchSleepInterval = WS::SettingPage.SettingsContainer.GetReactiveIntSetting(SettingsEnum.MaxFetchCount).ToReactivePropertyAsSynchronized(x => x.Value, x => x switch
             {
                 1 => n1,
                 2 => n2,
@@ -91,41 +77,20 @@ namespace Niconicome.ViewModels.Setting.Pages
                 4 => n4,
                 5 => n5,
                 _ => n4,
-            };
+            }, x => x.Value);
 
-            this.isExpandallPlaylistsEnableField = WS::SettingPage.SettingHandler.GetBoolSetting(SettingsEnum.ExpandAll);
-            this.isSavePrevPlaylistExpandedStateEnableField = WS::SettingPage.SettingHandler.GetBoolSetting(SettingsEnum.InheritExpandedState);
-            this.isStoreOnlyNiconicoIDEnableField = WS::SettingPage.SettingHandler.GetBoolSetting(SettingsEnum.StoreOnlyNiconicoID);
-            this.isAutoRenamingRemotePlaylistEnableField = WS::SettingPage.SettingHandler.GetBoolSetting(SettingsEnum.AutoRenameNetPlaylist);
-
-            this.IsSingletonWindowsEnable = new ReactiveProperty<bool>(WS::SettingPage.SettingHandler.GetBoolSetting(SettingsEnum.SingletonWindows));
-            this.IsSingletonWindowsEnable.Subscribe(value => WS::SettingPage.SettingHandler.SaveSetting(value, SettingsEnum.SingletonWindows)).AddTo(this.disposables);
-
-            this.IsConfirmngIfDownloadingEnable = new ReactiveProperty<bool>(WS::SettingPage.SettingHandler.GetBoolSetting(SettingsEnum.ConfirmIfDownloading));
-            this.IsConfirmngIfDownloadingEnable.Subscribe(value => WS::SettingPage.SettingHandler.SaveSetting(value, SettingsEnum.ConfirmIfDownloading)).AddTo(this.disposables);
-
+            this.IsSkippingSSLVerificationEnable = WS::SettingPage.SettingsContainer.GetReactiveBoolSetting(SettingsEnum.SkipSSLVerification);
+            this.IsExpandallPlaylistsEnable = WS::SettingPage.SettingsContainer.GetReactiveBoolSetting(SettingsEnum.ExpandAll);
+            this.IsSavePrevPlaylistExpandedStateEnable = WS::SettingPage.SettingsContainer.GetReactiveBoolSetting(SettingsEnum.InheritExpandedState);
+            this.IsStoreOnlyNiconicoIDEnable = WS::SettingPage.SettingsContainer.GetReactiveBoolSetting(SettingsEnum.StoreOnlyNiconicoID);
+            this.IsAutoRenamingRemotePlaylistEnable = WS::SettingPage.SettingsContainer.GetReactiveBoolSetting(SettingsEnum.AutoRenameNetPlaylist);
+            this.IsSingletonWindowsEnable = WS::SettingPage.SettingsContainer.GetReactiveBoolSetting(SettingsEnum.SingletonWindows);
+            this.IsConfirmngIfDownloadingEnable = WS::SettingPage.SettingsContainer.GetReactiveBoolSetting(SettingsEnum.ConfirmIfDownloading);
             this.SnackbarDuration = WS::SettingPage.SettingsContainer.GetReactiveIntSetting(SettingsEnum.SnackbarDuration, null, value => value <= 0 ? LocalConstant.DefaultSnackbarDuration : value);
+            this.IsShowingTasksAsTabEnable = WS::SettingPage.SettingsContainer.GetReactiveBoolSetting(SettingsEnum.ShowTasksAsTab);
         }
 
-        #region field
-        private bool isAutologinEnableField;
-
-        private bool isSkippingSSLVerificationEnableField;
-
-        private bool isSavePrevPlaylistExpandedStateEnableField;
-
-        private bool isExpandallPlaylistsEnableField;
-
-        private bool isStoreOnlyNiconicoIDEnableField;
-
-        private bool isAutoRenamingRemotePlaylistEnableField;
-
-        private string empty = string.Empty;
-
-        private ComboboxItem<int> maxFetchParallelCountField;
-
-        private ComboboxItem<int> fetchSleepIntervalFIeld;
-        #endregion
+        #region Props
 
         /// <summary>
         /// 選択可能な自動ログインの種別
@@ -140,7 +105,7 @@ namespace Niconicome.ViewModels.Setting.Pages
         /// <summary>
         /// Firefoxのプロファイル
         /// </summary>
-        public ReactiveProperty<string?> SelectedFirefoxProfileName { get; init; }
+        public ReactiveProperty<string> SelectedFirefoxProfileName { get; init; }
 
         /// <summary>
         /// Firefoxのプロファイルを表示するかどうか
@@ -148,10 +113,14 @@ namespace Niconicome.ViewModels.Setting.Pages
         public ReactiveProperty<bool> DisplayFirefoxPrifile { get; init; }
 
         /// <summary>
+        /// タスク一覧をタブ表示
+        /// </summary>
+        public ReactiveProperty<bool> IsShowingTasksAsTabEnable { get; init; }
+
+        /// <summary>
         /// 選択可能なFirefoxのプロファイル
         /// </summary>
         public List<string> SelectableFirefoxProfiles { get; init; }
-
 
         /// <summary>
         /// 同時取得数
@@ -166,52 +135,47 @@ namespace Niconicome.ViewModels.Setting.Pages
         /// <summary>
         /// 自動ログイン
         /// </summary>
-        public bool IsAutologinEnable { get => this.isAutologinEnableField; set => this.Savesetting(ref this.isAutologinEnableField, value, SettingsEnum.AutologinEnable); }
+        public ReactiveProperty<bool> IsAutologinEnable { get; init; }
 
         /// <summary>
         /// 動画情報最大並列取得数
         /// </summary>
-        public ComboboxItem<int> MaxFetchParallelCount
-        {
-            get => this.maxFetchParallelCountField;
-            set => this.Savesetting(ref this.maxFetchParallelCountField, value, SettingsEnum.MaxFetchCount);
-        }
+        public ReactiveProperty<ComboboxItem<int>> MaxFetchParallelCount { get; init; }
 
         /// <summary>
         /// 待機間隔
         /// </summary>
-        public ComboboxItem<int> FetchSleepInterval { get => this.fetchSleepIntervalFIeld; set => this.Savesetting(ref this.fetchSleepIntervalFIeld, value, SettingsEnum.FetchSleepInterval); }
+        public ReactiveProperty<ComboboxItem<int>> FetchSleepInterval { get; init; }
 
         /// <summary>
         /// SSL証明書の検証をスキップする
         /// </summary>
-        public bool IsSkippingSSLVerificationEnable { get => this.isSkippingSSLVerificationEnableField; set => this.Savesetting(ref this.isSkippingSSLVerificationEnableField, value, SettingsEnum.SkipSSLVerification); }
+        public ReactiveProperty<bool> IsSkippingSSLVerificationEnable { get; init; }
 
         /// <summary>
         /// 展開状況を保存する
         /// </summary>
-        public bool IsSavePrevPlaylistExpandedStateEnable { get => this.isSavePrevPlaylistExpandedStateEnableField; set => this.Savesetting(ref this.isSavePrevPlaylistExpandedStateEnableField, value, SettingsEnum.InheritExpandedState); }
+        public ReactiveProperty<bool> IsSavePrevPlaylistExpandedStateEnable { get; init; }
 
         /// <summary>
         /// すべて展開する
         /// </summary>
-        public bool IsExpandallPlaylistsEnable { get => this.isExpandallPlaylistsEnableField; set => this.Savesetting(ref this.isExpandallPlaylistsEnableField, value, SettingsEnum.ExpandAll); }
+        public ReactiveProperty<bool> IsExpandallPlaylistsEnable { get; init; }
 
         /// <summary>
         /// ニコニコ動画のIDのみを保存する
         /// </summary>
-        public bool IsStoreOnlyNiconicoIDEnable { get => this.isStoreOnlyNiconicoIDEnableField; set => this.Savesetting(ref this.isStoreOnlyNiconicoIDEnableField, value, SettingsEnum.StoreOnlyNiconicoID); }
+        public ReactiveProperty<bool> IsStoreOnlyNiconicoIDEnable { get; init; }
 
         /// <summary>
         /// 自動リネーム
         /// </summary>
-        public bool IsAutoRenamingRemotePlaylistEnable { get => this.isAutoRenamingRemotePlaylistEnableField; set => this.Savesetting(ref this.isAutoRenamingRemotePlaylistEnableField, value, SettingsEnum.AutoRenameNetPlaylist); }
+        public ReactiveProperty<bool> IsAutoRenamingRemotePlaylistEnable { get; init; }
 
         /// <summary>
         /// DL中は終了時に確認する
         /// </summary>
         public ReactiveProperty<bool> IsConfirmngIfDownloadingEnable { get; init; }
-
 
 
         /// <summary>
@@ -224,6 +188,8 @@ namespace Niconicome.ViewModels.Setting.Pages
         /// </summary>
         [RegularExpression(@"^\d$", ErrorMessage = "整数値を入力してください。")]
         public ReactiveProperty<int> SnackbarDuration { get; init; }
+
+        #endregion
 
     }
 
@@ -249,21 +215,21 @@ namespace Niconicome.ViewModels.Setting.Pages
             this.SelectableFirefoxProfiles = new List<string>() { ffp };
             this.SelectedFirefoxProfileName = new ReactiveProperty<string>(ffp);
 
-            this.MaxFetchParallelCount = n3;
-            this.FetchSleepInterval = n5;
+            this.MaxFetchParallelCount = new ReactiveProperty<ComboboxItem<int>>(n3);
+            this.FetchSleepInterval = new ReactiveProperty<ComboboxItem<int>>(n5);
         }
 
-        public bool IsAutologinEnable { set; get; } = true;
+        public ReactiveProperty<bool> IsAutologinEnable { set; get; } = new(true);
 
-        public bool IsSkippingSSLVerificationEnable { get; set; } = false;
+        public ReactiveProperty<bool> IsSkippingSSLVerificationEnable { get; set; } =new(false);
 
-        public bool IsSavePrevPlaylistExpandedStateEnable { get; set; } = true;
+        public ReactiveProperty<bool> IsSavePrevPlaylistExpandedStateEnable { get; set; } = new(true);
 
-        public bool IsExpandallPlaylistsEnable { get; set; } = true;
+        public ReactiveProperty<bool> IsExpandallPlaylistsEnable { get; set; } = new(true);
 
-        public bool IsStoreOnlyNiconicoIDEnable { get; set; } = true;
+        public ReactiveProperty<bool> IsStoreOnlyNiconicoIDEnable { get; set; } = new(true);
 
-        public bool IsAutoRenamingRemotePlaylistEnable { get; set; } = true;
+        public ReactiveProperty<bool> IsAutoRenamingRemotePlaylistEnable { get; set; } = new(true);
 
         public List<string> SelectableFirefoxProfiles { get; init; }
 
@@ -277,7 +243,9 @@ namespace Niconicome.ViewModels.Setting.Pages
 
         public ReactiveProperty<bool> IsSingletonWindowsEnable { get; init; } = new(true);
 
-        public ReactiveProperty<bool> IsConfirmngIfDownloadingEnable { get; init; } = new();
+        public ReactiveProperty<bool> IsConfirmngIfDownloadingEnable { get; init; } = new(true);
+
+        public ReactiveProperty<bool> IsShowingTasksAsTabEnable { get; init; } = new(true);
 
         public ReactiveProperty<int> SnackbarDuration { get; init; } = new();
 
@@ -285,9 +253,9 @@ namespace Niconicome.ViewModels.Setting.Pages
 
         public List<ComboboxItem<int>> SelectablefetchSleepInterval { get; init; }
 
-        public ComboboxItem<int> MaxFetchParallelCount { get; set; }
+        public ReactiveProperty<ComboboxItem<int>> MaxFetchParallelCount { get; set; }
 
-        public ComboboxItem<int> FetchSleepInterval { get; set; }
+        public ReactiveProperty<ComboboxItem<int>> FetchSleepInterval { get; set; }
 
     }
 }
