cd4399e42bd89d84bf8ca7167896ca1e3ec0e70e
Hayao-H <55943986+Hayao-H@users.noreply.github.com>
2022-03-16 21:27:46 +0900

    [追加]親コメントリストを実装

---
+++ Niconicome/Models/Domain/Niconico/Download/Comment/V2/Core/CommentCollection.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Niconicome.Models.Helper.Result;
+
+namespace Niconicome.Models.Domain.Niconico.Download.Comment.V2.Core
+{
+    public interface ICommentCollection
+    {
+
+        /// <summary>
+        /// コメント数を取得
+        /// </summary>
+        int Count { get; }
+
+        /// <summary>
+        /// 全てのコメントを取得する
+        /// </summary>
+        IEnumerable<IComment> Comments { get; }
+
+        /// <summary>
+        /// コメントを追加
+        /// </summary>
+        /// <param name="comment"></param>
+        /// <returns></returns>
+        IAttemptResult Add(IComment comment);
+
+        /// <summary>
+        /// コメントの欠け情報を取得する
+        /// </summary>
+        /// <returns></returns>
+        IReadOnlyList<UnFilledRange> GetUnFilledRange();
+    }
+
+    public class CommentCollection : ICommentCollection
+    {
+        public CommentCollection(int commentCount, int commentCountPerBlock)
+        {
+            this._commentCount = commentCount;
+            this._commentCountPerBlock = commentCountPerBlock;
+        }
+
+        #region field
+
+        private readonly List<IChildCommentCollection> _children = new();
+
+        private readonly int _commentCountPerBlock;
+
+        private readonly int _commentCount;
+
+        #endregion
+
+        #region Props
+
+        public int Count => this._children.Select(c => c.Count).Sum();
+
+        public IEnumerable<IComment> Comments => this._children.Select(c => c.Comments).SelectMany(_ => _);
+
+        #endregion
+
+        #region Method
+
+        public IAttemptResult Add(IComment comment)
+        {
+            string thread = comment.Thread;
+            int fork = comment.Fork;
+
+            IChildCommentCollection collection = this.GetCollection(thread, fork);
+
+            return collection.Add(comment);
+        }
+
+        public IReadOnlyList<UnFilledRange> GetUnFilledRange()
+        {
+            var list = new List<UnFilledRange>();
+
+            foreach (var collection in this._children)
+            {
+                IReadOnlyList<(IComment, int)> ranges = collection.GetUnFilledRange();
+
+                if (ranges.Count == 0) continue;
+                foreach (var range in ranges)
+                {
+                    (IComment comment, int count) = range;
+                    list.Add(new UnFilledRange(comment, count, collection.Thread, collection.Fork));
+                }
+            }
+
+            return list;
+        }
+
+        #endregion
+
+        #region field
+
+        /// <summary>
+        /// 指定したThread/Forkのコレクションが存在しない場合追加
+        /// </summary>
+        /// <param name="thread"></param>
+        /// <param name="fork"></param>
+        private void AddCollectionIfNotExists(string thread, int fork)
+        {
+            if (!this._children.Any(c => c.Thread == thread && c.Fork == fork))
+            {
+                this._children.Add(new ChildCommentCollection(this._commentCount, this._commentCountPerBlock, thread, fork));
+            }
+        }
+
+        /// <summary>
+        /// 指定したThread/Forkのコレクションを取得
+        /// </summary>
+        /// <param name="thread"></param>
+        /// <param name="fork"></param>
+        /// <returns></returns>
+        private IChildCommentCollection GetCollection(string thread, int fork)
+        {
+            this.AddCollectionIfNotExists(thread, fork);
+
+            return this._children.First(c => c.Thread == thread && c.Fork == fork);
+
+        }
+
+        #endregion
+    }
+
+    /// <summary>
+    /// コメントの欠け情報
+    /// </summary>
+    /// <param name="Start">欠けのスタート位置</param>
+    /// <param name="Count">欠けたコメントの数（コメント番号が小さい（=古い）方へ）</param>
+    /// <param name="Thread">Thread</param>
+    /// <param name="Fork">Fork</param>
+    public record UnFilledRange(IComment Start, int Count, string Thread, int Fork);
+}
---
+++ NiconicomeTest/NetWork/Download/Comment/V2/Comment/CommentCollectionUnitTest.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Niconicome.Models.Domain.Niconico.Download.Comment.V2.Core;
+using NUnit.Framework;
+using Core = Niconicome.Models.Domain.Niconico.Download.Comment.V2.Core;
+
+namespace NiconicomeTest.NetWork.Download.Comment.V2.Comment
+{
+    internal class CommentCollectionUnitTest
+    {
+        private Core::CommentCollection? _collection;
+
+        private List<Core::IComment>? _comments1;
+
+        private List<Core::IComment>? _comments2;
+
+        private readonly string _thread = "1";
+
+        private readonly int _fork = 0;
+
+        [OneTimeSetUp]
+        public void SetUpOnce()
+        {
+            this._comments1 = new List<Core.IComment>(Enumerable.Range(1, 10).Select(i => new Core::Comment()
+            {
+                No = i,
+                Fork = 0,
+                Thread = "1",
+                Content = "Hello World",
+            }));
+
+            this._comments2 = new List<Core.IComment>(Enumerable.Range(1, 10).Select(i => new Core::Comment()
+            {
+                No = i,
+                Fork = 0,
+                Thread = "2",
+                Content = "Hello World",
+            }));
+
+        }
+
+        [SetUp]
+        public void Setup()
+        {
+            this._collection = new Core::CommentCollection(10, 2);
+        }
+
+        [Test]
+        public void コメントを追加する()
+        {
+            foreach (var comment in this._comments1!)
+            {
+                this._collection!.Add(comment);
+            }
+
+            foreach (var comment in this._comments2!)
+            {
+                this._collection!.Add(comment);
+            }
+
+            Assert.That(this._collection!.Count, Is.EqualTo(20));
+        }
+
+        [Test]
+        public void コメントの欠けをチェックする()
+        {
+            //構造
+            //col1:[1,2][null,null][null,null][7,null][null,null]
+            //col2:[1,2][null,null][5,6][7,null][null,null]
+            foreach (var no in new[] { 1, 2, 7 }) this._collection!.Add(this._comments1![no - 1]);
+            foreach (var no in new[] { 1, 2, 5, 6, 7 }) this._collection!.Add(this._comments2![no - 1]);
+
+            Assert.That(this._collection!.Count, Is.EqualTo(8));
+
+            IReadOnlyList<UnFilledRange> unFilled = this._collection.GetUnFilledRange();
+
+            Assert.That(unFilled.Count, Is.EqualTo(2));
+
+            UnFilledRange range1 = unFilled[0];
+
+            Assert.That(range1.Thread, Is.EqualTo("1"));
+            Assert.That(range1.Fork, Is.EqualTo(0));
+            Assert.That(range1.Start.No, Is.EqualTo(7));
+            Assert.That(range1.Count, Is.EqualTo(4));
+
+            UnFilledRange range2 = unFilled[1];
+
+            Assert.That(range2.Thread, Is.EqualTo("2"));
+            Assert.That(range2.Fork, Is.EqualTo(0));
+            Assert.That(range2.Start.No, Is.EqualTo(5));
+            Assert.That(range2.Count, Is.EqualTo(2));
+
+        }
+    }
+}
