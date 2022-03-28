7d18a7b0f70f2b98c7b23036fd4ba9ba69fd689e
Hayao-H <55943986+Hayao-H@users.noreply.github.com>
2022-03-28 20:40:12 +0900

    [追加]コメントクラスを更新

---
+++ Niconicome/Models/Domain/Niconico/Net/Xml/Comment/V2/ChatElement.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AngleSharp.Dom;
+using System.Xml.Serialization;
+using System.ComponentModel;
+
+namespace Niconicome.Models.Domain.Niconico.Net.Xml.Comment.V2
+{
+    internal class ChatElement
+    {
+        [XmlAttribute(AttributeName = "thread")]
+        public string Thread { get; set; } = string.Empty;
+
+        [XmlAttribute(AttributeName = "no")]
+        public long No { get; set; }
+
+        [XmlAttribute(AttributeName = "vpos")]
+        public long Vpos { get; set; }
+
+        [XmlAttribute(AttributeName = "date")]
+        public long Date { get; set; }
+
+        [XmlAttribute(AttributeName = "date_usec")]
+        public int DateUsec { get; set; }
+
+        [XmlAttribute(AttributeName = "anonymity")]
+        public long Anonymity { get; set; }
+
+        [XmlAttribute(AttributeName = "user_id")]
+        public string? UserId { get; set; }
+
+        [XmlAttribute(AttributeName = "mail")]
+        public string? Mail { get; set; }
+
+        [XmlText]
+        public string? Text { get; set; }
+
+        [XmlAttribute(AttributeName = "leaf")]
+        public int Leaf { get; set; }
+
+        [XmlAttribute(AttributeName = "premium")]
+        public int? Premium { get; set; }
+
+        [XmlAttribute(AttributeName = "score")]
+        public int Score { get; set; }
+
+        [Browsable(false)]
+        [XmlIgnore]
+        public bool PremiumSpecified => Premium.HasValue;
+    }
+}
---
+++ Niconicome/Models/Domain/Niconico/Net/Xml/Comment/V2/PacketElement.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Niconicome.Models.Domain.Niconico.Net.Xml.Comment.V2
+{
+    [XmlRoot("packet")]
+    internal class PacketElement
+    {
+        [XmlElement("thread")]
+        public ThreadElement Thread { get; set; } = new();
+
+        [XmlElement("chat")]
+        public List<ChatElement> Chat { get; set; } = new();
+    }
+}
