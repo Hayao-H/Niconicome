5520de41e860e2f2346ecf9fd70e5a66b9aaea05
Hayao-H <55943986+Hayao-H@users.noreply.github.com>
2022-11-11 14:01:38 +0900

    [追加]ベースクラスを作成

---
+++ Niconicome/Models/Domain/Playlist/UpdatableInfoBase.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Niconicome.Models.Domain.Local.Store.V2;
+
+namespace Niconicome.Models.Domain.Playlist
+{
+    public class UpdatableInfoBase<TUpdater, TInfo> where TUpdater : IStoreUpdater<TInfo>
+    {
+        public UpdatableInfoBase(IStoreUpdater<TInfo> updater)
+        {
+            this._updater = updater;
+        }
+
+
+        #region field
+
+        private readonly IStoreUpdater<TInfo> _updater;
+
+        #endregion
+
+        protected void Update(TInfo infomation)
+        {
+            this._updater.Update(infomation);
+        }
+    }
+}
