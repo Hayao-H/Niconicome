ce1460af742143dfb9ce6d7514b03dc33299c7e2
Hayao-H <55943986+Hayao-H@users.noreply.github.com>
2023-03-21 23:31:40 +0900

    [追加]Xenoプレイリストの解析機能を実装

---
+++ Niconicome/Models/Domain/Local/DataBackup/Import/Xeno/Error/XenoDataParserError.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Niconicome.Models.Domain.Utils.Error;
+
+namespace Niconicome.Models.Domain.Local.DataBackup.Import.Xeno.Error
+{
+    public enum XenoDataParserError
+    {
+        [ErrorEnum(ErrorLevel.Error,"指定されたファイルが存在しません。(path:{0})")]
+        FileDoesNotExist,
+        [ErrorEnum(ErrorLevel.Error,"行の解析に失敗しました。(inpuit:{0})")]
+        FailedToParseLineOfRoot,
+        [ErrorEnum(ErrorLevel.Error,"レイヤーの取得に失敗しました。(input:{0})")]
+        FailedToParseayer,
+        [ErrorEnum(ErrorLevel.Error,"レイヤー構造に問題が存在するためノードの変換に失敗しました。(prevLayer:{0}, layer:{1})")]
+        FailedToConvertRootNodeToPlaylist,
+        [ErrorEnum(ErrorLevel.Error,"行の解析に失敗しました。(inpuit:{0})")]
+        FailedToParseLineOfPlaylist,
+        [ErrorEnum(ErrorLevel.Error,"動画情報の解析に失敗しました。")]
+        FailedToParseVideo,
+    }
+}
---
+++ Niconicome/Models/Domain/Local/DataBackup/Import/Xeno/StringContent/XenoDataParserSC.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Niconicome.Models.Domain.Utils.StringHandler;
+
+namespace Niconicome.Models.Domain.Local.DataBackup.Import.Xeno.StringContent
+{
+    public enum XenoDataParserSC
+    {
+        [StringEnum("ch.nicovideo.jp")]
+        ChannelURL,
+    }
+}
