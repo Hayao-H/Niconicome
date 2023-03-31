a687d1347dcc37b2ca6ca8adf18ee7957a3d28f6
Hayao-H <55943986+Hayao-H@users.noreply.github.com>
2023-03-31 23:26:46 +0900

    [追加]貼り付け機能を実装

--- NiconicomeWeb/src/videoList/main.ts
+++ NiconicomeWeb/src/videoList/main.ts
@@ -1,6 +1,7 @@
 import { DotNetObjectReference } from "../shared/DotNetObjectReference";
 import { ElementHandler, ElementHandlerImpl } from "../shared/ElementHandler";
 import { StyleHandler, StyleHandlerImpl } from "../shared/StyleHandler";
+import { SelectionHandlerImpl } from "./SelectionHandler/selectionHandler";
 import { SortHandler, SortHandlerImpl } from "./sortHandler/sortHandler";
 import { WidthHandler, WidthHandlerImpl } from "./widthHandler/widthHandler";
 
@@ -20,4 +21,11 @@ export async function setWidth(blazorView: DotNetObjectReference) {
     const widthHandler: WidthHandler = new WidthHandlerImpl(elmHandler, styleHandler, blazorView);
 
     await widthHandler.setWidth();
+}
+
+export function getSelectedIOfInput(): string {
+    const elmHandler: ElementHandler = new ElementHandlerImpl();
+    const handler = new SelectionHandlerImpl(elmHandler);
+
+    return handler.getSelected();
 }
