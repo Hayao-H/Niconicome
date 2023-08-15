b9d76e7173b2b30ffead0d1dadf4370d0197b93d
Hayao-H <55943986+Hayao-H@users.noreply.github.com>
2023-08-15 20:57:56 +0900

    [追加]KeyURIを追加

--- Niconicome/Models/Domain/Niconico/Watch/WatchSession.cs
+++ Niconicome/Models/Domain/Niconico/Watch/WatchSession.cs
@@ -18,7 +18,6 @@ namespace Niconicome.Models.Domain.Niconico.Watch
         Task GetVideoDataAsync(string id);
         bool IsSessionEnsured { get; }
         bool IsSessionExipired { get; }
-        string PlaylistUrl { get; }
         public IDomainVideoInfo? Video { get; }
         WatchSessionState State { get; }
         Task<Dmc::IStreamsCollection> GetAvailableStreamsAsync();
@@ -90,18 +89,6 @@ namespace Niconicome.Models.Domain.Niconico.Watch
         /// セッション失効フラグ
         /// </summary>
         public bool IsSessionExipired { get; private set; }
-
-        /// <summary>
-        /// プレイリストのURL
-        /// </summary>
-        public string PlaylistUrl
-        {
-            get
-            {
-                if (this.watchSessionInfo is null) throw new InvalidOperationException("セッション情報がnullのため、プレイリストのURLを取得できません。");
-                return this.watchSessionInfo.ContentUrl;
-            }
-        }
 
         /// <summary>
         /// 状態
@@ -215,7 +202,7 @@ namespace Niconicome.Models.Domain.Niconico.Watch
             if (this.watchSessionInfo is null) throw new InvalidCastException("セッション情報がnullのため、ストリームの一覧を取得できません。");
             var availableStreams = new List<Dmc::IStreamInfo>();
 
-            await this.GetAvailableStreamsAsync(this.PlaylistUrl, availableStreams);
+            await this.GetAvailableStreamsAsync(this.watchSessionInfo.ContentUrl, availableStreams);
 
             var streams = new Dmc::StreamsCollection();
             streams.AddRange(availableStreams);
@@ -279,12 +266,17 @@ namespace Niconicome.Models.Domain.Niconico.Watch
                 if (result.Data.DmcResponseJsonData is not string jsonData) return new AttemptResult<IWatchSessionInfo>() { Message = $"HeratBeatコンテントが正しくありません。" };
                 if (result.Data.ContentUrl is not string contentUrl) return new AttemptResult<IWatchSessionInfo>() { Message = $"コンテンツURLが正しくありません。" };
                 if (result.Data.SessionId is not string sessionID) return new AttemptResult<IWatchSessionInfo>() { Message = $"セッションIDが正しくありません。" };
+                if (result.Data.KeyURI is not string keyURI)
+                {
+                    keyURI = string.Empty;
+                }
 
                 var info = new WatchSessionInfo()
                 {
-                    DmcResponseJsonData = result.Data.DmcResponseJsonData,
-                    ContentUrl = result.Data.ContentUrl,
-                    SessionId = result.Data.SessionId,
+                    DmcResponseJsonData = jsonData,
+                    ContentUrl = contentUrl,
+                    SessionId = sessionID,
+                    KeyURI = keyURI,
                 };
 
                 return new AttemptResult<IWatchSessionInfo>() { IsSucceeded = true, Data = info };
