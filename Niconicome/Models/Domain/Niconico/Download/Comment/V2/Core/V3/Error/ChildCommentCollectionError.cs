adc35ba4067bc7e9120d0e8c9a7abd4c842609fa
Hayao-H <55943986+Hayao-H@users.noreply.github.com>
2024-09-15 23:43:26 +0900

    bug/comment (#657)
    * [変更]コメントの範囲チェックを追加
    
    * [変更]コメントコレクションのエラー処理を改善

---
+++ Niconicome/Models/Domain/Niconico/Download/Comment/V2/Core/V3/Error/ChildCommentCollectionError.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Niconicome.Models.Domain.Utils.Error;
+
+namespace Niconicome.Models.Domain.Niconico.Download.Comment.V2.Core.V3.Error
+{
+    public enum ChildCommentCollectionError
+    {
+        [ErrorEnum(ErrorLevel.Error, "コメントのThreadまたはForkがコレクションのThreadまたはForkと一致しません。")]
+        InvalidAddParameter,
+        [ErrorEnum(ErrorLevel.Error, "コメント番号が範囲外です。")]
+        CommentIndexOutOfRange,
+        [ErrorEnum(ErrorLevel.Error, "コメントが存在しません。(No:{0})")]
+        CommentNotFound,
+        [ErrorEnum(ErrorLevel.Error, "最初のコメントを取得できませんでした。コメントが空の可能性があります。")]
+        CannotGetFirstComment,
+    }
+}
